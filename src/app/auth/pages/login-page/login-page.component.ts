9ab535c0d18eb5617cc40c551877dad40008e4e2
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2023-10-19 01:25:34 -0300

    validacion de login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2'
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -8,6 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class LoginPageComponent {
 
   private fb = inject(FormBuilder);
+  private authService = inject( AuthService);
 
   public myForm: FormGroup = this.fb.group({
     email:    ['', [ Validators.required, Validators.email ]],
@@ -15,6 +19,14 @@ export class LoginPageComponent {
   })
 
   login(){
-    console.log(this.myForm.value)
+    const {email, password} = this.myForm.value;
+    console.log(this.myForm.value);
+    this.authService.login(email,password)
+    .subscribe({
+      next: () => console.log('todo bien'),
+      error: (message) => {
+        Swal.fire('Error', message, 'error')
+      }
+    });
   }
 }
