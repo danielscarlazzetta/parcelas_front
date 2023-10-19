b4fe4770980459dfad3e956ea1446b1121efacc8
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2023-10-19 00:07:27 -0300

    creacion formulario, falta conectarlko a la db

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myFormRegister: FormGroup = this.fb.group({
+    name:['', [ Validators.required, Validators.pattern(/^[A-Za-z]+$/)]],
+    email:['', [ Validators.required, Validators.email ]],
+    password:['', [ Validators.required, Validators.minLength(6)]],
+  })
+
+  register(){
+    console.log(this.myFormRegister.value)
+  }
 
 }
