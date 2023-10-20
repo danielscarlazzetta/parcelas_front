4a3e74fc41a9ce28fce7891d15eb576c410c3d27
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2023-10-19 22:01:42 -0300

    mostrar datos en login y probar funcionamiento de comparacion

--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, OnInit, computed, inject } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { AuthStatus } from '../../../auth/interfaces/auth-status.enum';
 
@@ -6,11 +6,22 @@ import { AuthStatus } from '../../../auth/interfaces/auth-status.enum';
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.css']
 })
-export class DashboardLayoutComponent {
+export class DashboardLayoutComponent implements OnInit {
 
   private userService= inject(AuthService);
   public user = computed( () => this.userService.currentUser())
   // get user(){
   //   return this.userService.currentUser();  
   // }
+
+
+  ngOnInit(): void {
+    this.prueba_de_rol()
+  }
+
+  prueba_de_rol(){
+    if(this.user()?.name === 'daniel'){
+      console.log('prueba para mostrar distintos landing page')
+    }
+  }
 }
